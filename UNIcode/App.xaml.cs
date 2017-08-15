718518fb431c2fcff347552bdc1f96551ad0a670
vainamov <vainamov@users.noreply.github.com>
2017-08-15 15:45:39 +0200

    Initial commit

---
+++ UNIcode/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace UNIcode
+{
+    /// <summary>
+    ///     Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    { }
+}
---
+++ UNIcode/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace UNIcode
+{
+    /// <summary>
+    ///     Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}

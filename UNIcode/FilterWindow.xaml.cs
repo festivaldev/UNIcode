1ef812efec430a860514a757e7489aaf419a3785
vainamov <vainamov@users.noreply.github.com>
2017-08-15 19:07:56 +0200

    Reconstructed FilterWindow

---
+++ UNIcode/FilterWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Unicode;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UNIcode
+{
+    public partial class FilterWindow : Window
+    {
+        #region Properties
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Ctors
+
+        public FilterWindow() {
+            InitializeComponent();
+
+            foreach (Label label in wrpExamples.Children) {
+                label.Cursor = Cursors.Hand;
+                label.MouseDown += OnFilterExampleMouseDown;
+            }
+        }
+
+        #endregion
+
+        #region Public Functions
+
+        public void Show(MainWindow owner) {
+            this.Owner = owner;
+
+            cbxBlocks.ItemsSource = UnicodeInfo.GetBlocks().Select(b => $"{b.Name} [{b.CodePointRange.ToString().Replace("..", "-")}]");
+            cbxCategories.ItemsSource = Enum.GetNames(typeof(UnicodeCategory));
+
+            cbxBlocks.SelectedIndex = cbxCategories.SelectedIndex = 0;
+            chxBlockApplies.IsChecked = chxCategoryApplies.IsChecked = false;
+
+            Show();
+        }
+
+        #endregion
+
+        #region Private Functions
+
+        #endregion
+
+        #region Event Handlers
+
+        private void OnApplyClick(object sender, EventArgs e) {
+            ((MainWindow) Owner).ApplyFilter(tbxFilter.Text, chxBlockApplies.IsChecked.Value ? cbxBlocks.SelectedItem.ToString().Split('[')[0].Trim() : string.Empty, (UnicodeCategory) Enum.Parse(typeof(UnicodeCategory), cbxCategories.SelectedItem.ToString()), chxCategoryApplies.IsChecked.Value);
+            Close();
+        }
+
+        private void OnBlocksSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            chxBlockApplies.IsChecked = true;
+        }
+
+        private void OnCategoriesSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            chxCategoryApplies.IsChecked = true;
+        }
+
+        private void OnFilterExampleMouseDown(object sender, MouseEventArgs e) {
+            var el = (Label) sender;
+            tbxFilter.Text = el.Content.ToString();
+        }
+
+        private void OnResetClick(object sender, EventArgs e) {
+            ((MainWindow) Owner).ResetFilter();
+            Close();
+        }
+
+        #endregion
+    }
+}
--- UNIcode/MainWindow.xaml.cs
+++ UNIcode/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Unicode;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -40,6 +42,7 @@ namespace UNIcode
         private List<int> filteredCharacters = new List<int>();
         private double rowCount = 5D;
         private FontFamily selectedFont;
+        private int _tileSize;
         private int tileSize = 70;
 
         #endregion
@@ -51,20 +54,68 @@ namespace UNIcode
 
             LoadAllFontFamilies();
             cbxFamilies.ItemsSource = FontFamilies;
-            cbxTileSize.ItemsSource = Helper.GetRangeInSteps(30, 300, 10);
+            cbxTileSize.ItemsSource = Helper.GetRangeInSteps(30, 400, 10);
         }
 
         #endregion
 
         #region Public Functions
 
+        public void ApplyFilter(string filter, string block, UnicodeCategory category, bool categoryApplies) {
+            filteredCharacters = new List<int>(characters);
+
+            if (filter.StartsWith(":")) {
+                try {
+                    filter = filter.Remove(0, 1);
+                    if (filter.Contains('-')) {
+                        var range = filter.Split('-').Select(s => Convert.ToInt32(s, 16)).ToArray();
+                        filteredCharacters = filteredCharacters.FindAll(c => c >= range[0] && c <= range[1]);
+                    } else if (filter.Contains(',')) {
+                        var items = filter.Split(',').Select(s => Convert.ToInt32(s, 16));
+                        filteredCharacters = filteredCharacters.Intersect(items).ToList();
+                    } else {
+                        filteredCharacters = filteredCharacters.FindAll(c => c == Convert.ToInt32(filter, 16));
+                    }
+                } catch { }
+            } else {
+                if (!string.IsNullOrEmpty(filter)) {
+                    filteredCharacters = filteredCharacters.FindAll(c => UnicodeInfo.GetName(c)?.Contains(filter.ToUpper()) ?? false);
+                }
+                if (!string.IsNullOrEmpty(block)) {
+                    filteredCharacters = filteredCharacters.FindAll(c => UnicodeInfo.GetBlockName(c) == block);
+                }
+                if (categoryApplies) {
+                    filteredCharacters = filteredCharacters.FindAll(c => UnicodeInfo.GetCharInfo(c).Category == category);
+                }
+            }
+
+            if (filteredCharacters.Count == characters.Count) {
+                ResetFilter();
+            } else if (filteredCharacters.Count == 0) {
+                ResetFilter();
+                MessageBox.Show("No characters matched your filter.", "UNI", MessageBoxButton.OK, MessageBoxImage.Error);
+                Focus();
+            } else {
+                currentStartIndex = 0;
+                ResetScrollbar();
+                ShowGlyphs();
+            }
+        }
+
+        public void ResetFilter() {
+            filteredCharacters.Clear();
+            currentStartIndex = 0;
+            ResetScrollbar();
+            ShowGlyphs();
+        }
+
         #endregion
 
         #region Private Functions
 
         private void AdjustDimension() {
-            wrpGlyphes.Width = wrpGlyphes.MaxWidth = tileSize * columnCount;
-            wrpGlyphes.Height = wrpGlyphes.MaxHeight = tileSize * rowCount;
+            wrpGlyphs.Width = wrpGlyphs.MaxWidth = tileSize * columnCount;
+            wrpGlyphs.Height = wrpGlyphs.MaxHeight = tileSize * rowCount;
 
             CreateGlyphTable();
             ShowGlyphs();
@@ -80,9 +131,10 @@ namespace UNIcode
                     var newColumnCount = Math.Floor(width / tileSize);
                     var newRowCount = Math.Floor(height / tileSize);
 
-                    if (newColumnCount == columnCount && newRowCount == rowCount)
+                    if (newColumnCount == columnCount && newRowCount == rowCount && _tileSize == tileSize)
                         return;
 
+                    _tileSize = tileSize;
                     columnCount = newColumnCount;
                     rowCount = newRowCount;
                     tbxDimension.Text = $"{columnCount}x{rowCount}";
@@ -94,21 +146,21 @@ namespace UNIcode
         }
 
         private void CenterGlyphTable() {
-            var left = (grdGrid.ActualWidth - wrpGlyphes.ActualWidth) / 2;
-            Canvas.SetLeft(wrpGlyphes, left);
+            var left = (grdGrid.ActualWidth - wrpGlyphs.ActualWidth) / 2;
+            Canvas.SetLeft(wrpGlyphs, left);
 
-            var top = (grdGrid.ActualHeight - wrpGlyphes.ActualHeight) / 2;
-            Canvas.SetTop(wrpGlyphes, top);
+            var top = (grdGrid.ActualHeight - wrpGlyphs.ActualHeight) / 2;
+            Canvas.SetTop(wrpGlyphs, top);
         }
 
         private void CreateGlyphTable() {
-            foreach (Label label in wrpGlyphes.Children) {
+            foreach (Label label in wrpGlyphs.Children) {
                 label.MouseDown -= OnLabelMouseDown;
                 label.MouseEnter -= OnLabelMouseEnter;
                 label.MouseLeave -= OnLabelMouseLeave;
             }
 
-            wrpGlyphes.Children.Clear();
+            wrpGlyphs.Children.Clear();
 
             for (var i = 0; i < (int) columnCount * rowCount; i++) {
                 var label = new Label {
@@ -130,7 +182,7 @@ namespace UNIcode
                 label.MouseEnter += OnLabelMouseEnter;
                 label.MouseLeave += OnLabelMouseLeave;
 
-                wrpGlyphes.Children.Add(label);
+                wrpGlyphs.Children.Add(label);
             }
 
             ResetScrollbar();
@@ -163,7 +215,7 @@ namespace UNIcode
         }
 
         private async void ShowGlyphs() {
-            var count = wrpGlyphes.Children.Count;
+            var count = wrpGlyphs.Children.Count;
             var data = new List<(int i, string content, string toolTip)>();
 
             await Task.Run(() => {
@@ -188,7 +240,7 @@ namespace UNIcode
             });
 
             foreach (var element in data) {
-                var label = (Label) wrpGlyphes.Children[element.i];
+                var label = (Label) wrpGlyphs.Children[element.i];
                 label.Content = element.content;
                 label.ToolTip = element.toolTip;
             }
@@ -214,14 +266,27 @@ namespace UNIcode
             cbxTypefaces.SelectedIndex = 0;
 
             LoadFontGlyphs();
+            OnTypefaceChanged(cbxTypefaces, new SelectionChangedEventArgs(e.RoutedEvent, new List<object>(), new List<object>()));
         }
 
         private void OnFormKeyDown(object sender, KeyEventArgs e) {
-            // TODO
+            if (e.Key == Key.F && (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))) {
+                var window = new FilterWindow();
+                window.Show(this);
+            }
         }
 
         private void OnInputKeyDown(object sender, KeyEventArgs e) {
-
+            if (e.Key == Key.Enter) {
+                try {
+                    var dimensions = tbxDimension.Text.ToLower().Split('x').Select(int.Parse).ToList();
+                    if (columnCount != dimensions[0] || rowCount != dimensions[1]) {
+                        columnCount = dimensions[0];
+                        rowCount = dimensions[1];
+                        AdjustDimension();
+                    }
+                } catch { }
+            }
         }
 
         private void OnLabelMouseDown(object sender, MouseButtonEventArgs e) {
@@ -248,18 +313,38 @@ namespace UNIcode
         }
 
         private void OnLoaded(object sender, EventArgs e) {
+            cbxFamilies.SelectedIndex = 0;
+            if (Environment.GetCommandLineArgs().Length >= 2) {
+                try {
+                    cbxFamilies.SelectedItem = Environment.GetCommandLineArgs()[1];
+                } catch { }
+            }
 
+            cbxTypefaces.SelectedIndex = 0;
+            if (Environment.GetCommandLineArgs().Length >= 3) {
+                try {
+                    cbxTypefaces.SelectedItem = Environment.GetCommandLineArgs()[2];
+                } catch { }
+            }
+
+            cbxTileSize.SelectedIndex = 4;
+            CalculateDimension();
         }
 
         private void OnMouseWheel(object sender, MouseWheelEventArgs e) {
-
+            scbVertical.Value += e.Delta < 0 ? 1 : -1;
+            OnScroll(scbVertical, new ScrollEventArgs(ScrollEventType.SmallIncrement, scbVertical.Value));
         }
 
         private void OnScroll(object sender, ScrollEventArgs e) {
-
+            try {
+                currentStartIndex = (int) e.NewValue * (int) columnCount;
+                ShowGlyphs();
+            } catch { }
         }
 
         private void OnTileSizeChanged(object sender, SelectionChangedEventArgs e) {
+            _tileSize = tileSize;
             tileSize = (int) cbxTileSize.SelectedItem;
             if (autoSizeEnabled) {
                 CalculateDimension();
@@ -272,9 +357,11 @@ namespace UNIcode
             selectedFont = new FontFamily($"{cbxFamilies.SelectedItem} {cbxTypefaces.SelectedItem}");
             tbxString.FontFamily = selectedFont;
 
-            foreach (Label label in wrpGlyphes.Children) {
+            foreach (Label label in wrpGlyphs.Children) {
                 label.FontFamily = selectedFont;
             }
+
+            ShowGlyphs();
         }
 
         private void OnUnchecked(object sender, RoutedEventArgs e) {

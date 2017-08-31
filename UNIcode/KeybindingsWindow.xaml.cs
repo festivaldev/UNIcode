04353c6c708eb357d86cdcc3cdbb97611942a46e
vainamov <vainamov@users.noreply.github.com>
2017-08-31 19:31:03 +0200

    KeybindingsWindow Codebehind Cleanup

--- UNIcode/KeybindingsWindow.xaml.cs
+++ UNIcode/KeybindingsWindow.xaml.cs
@@ -1,26 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace UNIcode
 {
-    /// <summary>
-    /// Interaktionslogik für KeybindingsWindow.xaml
-    /// </summary>
     public partial class KeybindingsWindow : Window
     {
+        #region Properties
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Ctors
+
         public KeybindingsWindow() {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Public Functions
+
+        #endregion
+
+        #region Private Functions
+
+        #endregion
+
+        #region Event Handlers
+
+        #endregion
     }
 }
